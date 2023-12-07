4df203d6f29090f3dedac270f7527d0262a942b6
Priyanshu <Priyanshukumarofficials@gmail.com>
2023-12-07 22:05:10 +0530

    Fixing Errors

--- backend/data/database.js
+++ backend/data/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI, {
         dbName: 'Notes',
-    }).then(() => {
+    }).then((c) => {
         console.log("Database Connected!");
     }).catch((error) => {
         console.log(error);
